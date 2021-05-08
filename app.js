38d1bf2ba85f3acf3415589ed4004b94db6e617c
vishalsinghji <vishalkmr272@gmail.com>
2021-05-08 15:17:59 +0530

    port changed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv/config');
+require('dotenv').config();
 const authJwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/error-handler');
 var PORT =process.env.PORT || 5000;
